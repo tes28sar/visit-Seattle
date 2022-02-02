544fb67963fe66caf3aa418ce33b2bbb858f9a0f
Sara Teshome <tes-sar@hotmail.com>
2022-02-01 21:01:25 -0800

    js

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log('Hello World');
+
+let city = 'seattle'; 
+let age = 171; 
+
+console.log(city);
+console.log(age);
+
+let rating = 4.5;
+let isFun = true; 
+
+console.log(rating);
+
+// let name = prompt('what is the city name?');/ 
+// console.log(name)
+
+function hello(){
+    let name= prompt('what is the city name?')
+if(name.toUpperCase()=== "SEATTLE"){
+    alert("welcome")
+}else{
+    alert("no, you are not right, it's seattle")
+}
+}; 
+
+
+console.log(name)
+hello(); 
